bedad3a6f99485cdd1d53387e65ed43b544c39f5
0fatal <2816813070@qq.com>
2024-03-08 16:09:13 +0800

    default lang to zh

--- web/src/utils/i18n.ts
+++ web/src/utils/i18n.ts
@@ -16,6 +16,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: "zh",
+    lng: 'zh',
     debug: process.env.NODE_ENV === "development",
 
     interpolation: {
