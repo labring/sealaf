dd6ef3f0db5969aa0d7c6d1fbb4da930532f77b5
limbo <huahua1319873800@outlook.com>
2024-11-18 12:01:19 +0800

    chore(web):i18n sync with sealos desktop (#18)

--- web/src/pages/auth/index.tsx
+++ web/src/pages/auth/index.tsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Center, Spinner } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
-import { SessionV1 } from "@zjy365/sealos-desktop-sdk/*";
-import { createSealosApp, sealosApp } from "@zjy365/sealos-desktop-sdk/app";
 
 import useSessionStore from "./store";
 
 import { AuthenticationControllerSignin } from "@/apis/v1/auth";
 
 const AuthPage = () => {
-  const { session, setSession, getKubeconfig, getNamespace } = useSessionStore();
-  const [isInit, setIsInit] = useState(false);
-
-  useEffect(() => {
-    return createSealosApp();
-  }, []);
-
-  useEffect(() => {
-    const initApp = async () => {
-      const sealosUser = import.meta.env.VITE_SEALOS_MOCK_USER;
-      const sealosKc = import.meta.env.VITE_SEALOS_MOCK_KC;
-
-      if (sealosUser && sealosKc) {
-        const testSession: SessionV1 = {
-          user: {
-            id: "",
-            name: sealosUser,
-            avatar: "",
-            k8sUsername: "",
-            nsid: "",
-          },
-          kubeconfig: sealosKc,
-        };
-        setSession(testSession);
-        return;
-      }
-
-      const result = await sealosApp.getSession();
-
-      setSession(result);
-    };
-
-    initApp().finally(() => setIsInit(true));
-  }, []);
+  const navigate = useNavigate();
+  const { session, getKubeconfig, getNamespace } = useSessionStore();
 
   const { mutateAsync: signin } = useMutation(["signin"], () => {
     return AuthenticationControllerSignin({
@@ -52,23 +19,35 @@ const AuthPage = () => {
     });
   });
 
-  useEffect(() => {
-    if (!isInit) return;
-
+  const handleAuth = async () => {
     const localNamespace = localStorage.getItem("sealos-namespace");
     const localToken = localStorage.getItem("token");
     const namespace = getNamespace();
 
     if (session.user && namespace && (localNamespace !== namespace || !localToken)) {
-      signin().then((res) => {
+      try {
+        const res = await signin();
         localStorage.setItem("token", res?.data.token);
         localStorage.setItem("sealos-namespace", namespace);
-        window.location.href = "/dashboard";
-      });
+        navigate("/dashboard");
+      } catch (error) {
+        if (error instanceof Error) {
+          // eslint-disable-next-line no-console
+          console.error("login error:", error.message);
+        } else {
+          // eslint-disable-next-line no-console
+          console.error("login error:", error);
+        }
+      }
     } else if (localToken && namespace && localNamespace === namespace) {
-      window.location.href = "/dashboard";
+      navigate("/dashboard");
     }
-  }, [session, isInit]);
+  };
+
+  // after signin, handle auth and goto dashboard
+  useEffect(() => {
+    handleAuth();
+  }, [session]);
 
   return (
     <Center w="100vw" h="100vh">
